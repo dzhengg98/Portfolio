cbfa37a84a2413ece339e97f6d4cc19be9a9856c
David Zheng <dzheng3456@gmail.com>
2022-07-17 14:09:22 -0400

    working on portfolio

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -6,9 +6,9 @@ import { SiAngellist } from 'react-icons/si';
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
-      <a className='header__links' href="https://linkedin.com/in/david-zheng-87671b237/" target="_blank" rel="noreferrer noopener"><BsLinkedin size={24}/></a>
-      <a className='header__links' href="https://github.com/dzhengg98" target="_blank" rel="noreferrer noopener"><FaGithubSquare size={28}/></a>
-      <a className='header__links' href="https://angel.co/u/david-zheng-27" target="_blank" rel="noreferrer noopener"><SiAngellist size={24}/></a>
+      <a className='header__links' href="https://linkedin.com/in/david-zheng-87671b237/" target="_blank" rel="noreferrer noopener"><BsLinkedin size={34}/></a>
+      <a className='header__links' href="https://github.com/dzhengg98" target="_blank" rel="noreferrer noopener"><FaGithubSquare size={38}/></a>
+      <a className='header__links' href="https://angel.co/u/david-zheng-27" target="_blank" rel="noreferrer noopener"><SiAngellist size={34}/></a>
     </div>
   )
 }
